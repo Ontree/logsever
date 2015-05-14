0caa29024b208bcbdcfbbcfbbe68bb6c1b1e3304
zouhaofeng <zhfthss@126.com>
2015-05-14 20:20:24 +0800

    merge global.java

--- src/edu/thu/ss/logserver/Global.java
+++ src/edu/thu/ss/logserver/Global.java
@@ -3,6 +3,7 @@ package edu.thu.ss.logserver;
 public class Global {
 	static String LogFileName;
 	
+	public static String LOG_FILE_PATH;
 	public final static int EMPLOYEE = 1;
 	public final static int INTERVAL = 2;
 	public final static int STATUS = 3;
